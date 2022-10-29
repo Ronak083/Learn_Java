29673bc212fd46b570380697387b39263c6f4f44
ronak <ronakgupta.official@gmail.com>
2022-10-29 23:03:08 +0530

    Initial commit Dheeru Manduru

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        SDA array = new SDA(10);
+        array.Insert(2, 50);
+        array.Insert(4, 540);
+        array.Insert(2, 54);
+        array.Insert(20, 554);
+        array.Insert(7, 56);
+
+        array.Traverse();
+        array.Search(540);
+    }
+}
