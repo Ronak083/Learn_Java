8935f45b76b892803fa4f39b9ab889f833525149
ronak <ronakgupta.official@gmail.com>
2022-12-17 15:04:35 +0530

    update Circular Linked list

--- src/Data_Structures/LinkedList/CircularLL.java
+++ src/Data_Structures/LinkedList/CircularLL.java
@@ -25,7 +25,36 @@ public class CircularLL {
             size++;
         }
     }
-    public void insert(int location, int value){}
+    public void insert(int location, int value){
+
+        if (head == null){
+            createcll(value);
+            return;
+        }
+        Node node = new Node();
+        node.value = value;
+        if (location == 0) {
+            node.next = head;
+            tail.next = node;
+            head = node;
+        } else if (location >= size) {
+           node.next = head;
+           tail.next = node;
+           tail = node;
+        } else {
+            Node temp = head;
+            int index = 0;
+            while (index<location-1){
+                temp = temp.next;
+                index++;
+            }
+            Node next = temp.next;
+            temp.next = node;
+            node.next = next;
+        }
+        size++;
+
+    }
     public void traverse(){
         if (head == null){
             return;
@@ -36,12 +65,29 @@ public class CircularLL {
             System.out.print(temp.value + "->");
             temp = temp.next;
         }
-        System.out.print(temp.value + "->");
+        System.out.print(temp.value + "\n");
     }
-    public void search(int value){}
-    public void delete(int location){}
-    public void deleteCll(
-    head = null;
-    tail = null;
-    ){}
+    public void search(int value){
+        if(head != null){
+            Node temp = head;
+            for (int i =0; i<size;i++){
+                if(temp.value == value){
+                    System.out.println("Found Successfully "+ value+" at location " + i + "\n");
+                    return;
+                }
+                temp= temp.next;
+            }
+            System.out.println("Node not found " + value +"\n");
+            return;
+        }
+    }
+    public void delete(int location){
+
+    }
+    public void deleteCll() {
+        head = null;
+        tail = null;
+        System.out.println( "List deleted successfully\n");
+    }
+
 }
--- src/Data_Structures/LinkedList/Main.java
+++ src/Data_Structures/LinkedList/Main.java
@@ -24,9 +24,17 @@ public class Main {
 
       CircularLL cll = new CircularLL();
       cll.createcll(1);
-      //cll.insert(2);
-      //cll.insert(3);
-cll.traverse();
+      cll.insert(2);
+      cll.insert(3);
+      cll.insert(5);
+      cll.traverse();
+      cll.insert(0,0);
+      cll.insert(6,6);
+      cll.insert(4,4);
+      cll.traverse();
+      cll.search(5);
+      cll.search(7);
+
 
 
         }
