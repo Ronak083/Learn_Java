356aaa3bae1060dc6437e58cdeda616fc2de8236
ronak <ronakgupta.official@gmail.com>
2022-12-18 18:33:14 +0530

    ..

--- src/Data_Structures/Recursion/fibo.java
+++ src/Data_Structures/Recursion/fibo.java
@@ -1,13 +1,20 @@
-package Data_Structures.Recursion;
+package LearnRecursion;
+
 public class fibo {
+
     public static void main(String[] args) {
         fibo recursion = new fibo();
         var rec = recursion.fibonacci(10);
         System.out.println(rec);
     }
+
     public int fibonacci(int n){
         if(n<0) return -1;
         if(n==0 || n==1) return n;
         return fibonacci(n-1) + fibonacci(n-2);
     }
+
+
+
+
 }
