fdcc67f31dd9f72748b2e5a8f6bbda1d539a5411
ronak <ronakgupta.official@gmail.com>
2022-12-19 17:14:35 +0530

    19/12 Created Stack operation

---
+++ src/Data_Structures/Stack/StackUsingArray.java
@@ -0,0 +1,21 @@
+package Data_Structures.Stack;
+public class StackUsingArray {
+    int[] arr;
+    int topOfStack;
+    public StackUsingArray(int size){
+        this.arr = new int[size];
+        this.topOfStack = -1;
+        System.out.println("Stack is created Successfully");
+    }
+
+    public boolean isEmpty() {
+        if (topOfStack == -1) return true;
+        else return false;
+    }
+
+    public boolean isFull(){
+        if (topOfStack == arr.length-1) return true;
+        else return false;
+    }
+
+}
