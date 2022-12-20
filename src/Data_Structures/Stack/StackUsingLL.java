fed957ab68070396a15d656e1c96a9f760121634
ronak <ronakgupta.official@gmail.com>
2022-12-20 23:06:04 +0530

    20/12 03 Add Stack operation using LInked List

--- src/Data_Structures/Stack/StackUsingLL.java
+++ src/Data_Structures/Stack/StackUsingLL.java
@@ -1,4 +1,24 @@
 package Data_Structures.Stack;
 
 public class StackUsingLL {
+
+    public boolean isEmpty(){
+
+    }
+    public boolean isFull(){
+
+    }
+    public void push(int value){
+
+    }
+    public int pop(){
+        return -1;
+    }
+    public int top(){
+        return -1;
+    }
+    public void delete(){
+
+    }
+
 }
