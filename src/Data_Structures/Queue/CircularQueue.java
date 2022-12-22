06c76c6915a67ac516f3a820a7ffe9154fb15e24
ronak <ronakgupta.official@gmail.com>
2022-12-22 23:43:29 +0530

    22/12 03 Circular  Queue using Array

--- src/Data_Structures/Queue/CircularQueue.java
+++ src/Data_Structures/Queue/CircularQueue.java
@@ -11,12 +11,52 @@ public class CircularQueue {
         this.end = -1;
         System.out.println("Successfully Created CQ");
     }
-    public void enqueue(int value){}
-    public void dequeue(){}
-    public boolean isEmpty(){return false;}
-    public boolean isFull(){return false;}
+    public void enqueue(int value){
+        if (isFull()) System.out.println("Queue is Full");
+        else if (isEmpty()) {
+            start =0;
+            end++;
+            arr[end] = value;
+            System.out.println("Successfully Inserted");
+        }
+        else {
+            if(end+1== size) end = 0;
+            else end++;
+            arr[end] = value;
+            System.out.println("Successfully Inserted");
+        }
+    }
+    public int dequeue(){
+        if (isEmpty()){
+            System.out.println("The CQ is Empty");
+            return -1;
+        } else{
+            int result = arr[start];
+            arr[start] =0 ;
+            if (start == end) start = end = -1;
+            else if (start + 1 == size) start = 0;
+            else start++;
+            return result;
+        }
+    }
+    public boolean isEmpty(){
+        if(end == -1) return true;
+        return false;
+    }
+    public boolean isFull(){
+        if (end+1 == start) {
+            return true;
+        }
+        else if (start == 0 && end+1 == size){
+            return true;
+        }
+        return false;
+    }
     public int top(){
-        return arr[start];
+        if (isEmpty()){
+            System.out.println("The CQ is Empty");
+            return -1;
+        } else return arr[start];
     }
     public void delete(){
         arr = null;
--- src/Data_Structures/Queue/QueueUsingLL.java
+++ src/Data_Structures/Queue/QueueUsingLL.java
@@ -1,4 +1,7 @@
 package Data_Structures.Queue;
 
 public class QueueUsingLL {
+    public QueueUsingLL(int size){
+
+    }
 }
