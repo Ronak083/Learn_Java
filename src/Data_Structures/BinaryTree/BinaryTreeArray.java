a294c4fc3e0d440b3dad089d08dcc159e6375050
ronak <ronakgupta.official@gmail.com>
2022-12-31 10:16:16 +0530

    31/12 Completed array Binary Tree

--- src/Data_Structures/BinaryTree/BinaryTreeArray.java
+++ src/Data_Structures/BinaryTree/BinaryTreeArray.java
@@ -14,12 +14,62 @@ public class BinaryTreeArray {
             return true;
         } else return false;
     }
-
     void insert(String value){
         if (!isFull()){
             arr[lastusedIndex+1] = value;
             lastusedIndex++;
             System.out.println("Node Inserted successfully");
         }else System.out.println("The BT is full");
+    }
+    void preorder(int index){
+        if(index> lastusedIndex) return;
+        System.out.print(arr[index] + " ");
+        preorder(index*2);
+        preorder(index*2 + 1);
+    }
+    void Inorder(int index) {
+        if (index > lastusedIndex) return;
+        Inorder(index*2);
+        System.out.print(arr[index] + " ");
+        Inorder(index*2 + 1);
+    }
+    void postorder(int index) {
+        if (index > lastusedIndex) return;
+        postorder(index*2);
+        postorder(index*2 + 1);
+        System.out.print(arr[index] + " ");
+    }
+    void levelorder(int index){
+        for (int i =1;i<=lastusedIndex;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public int search(String value){
+        for (int i =1;i<=lastusedIndex;i++){
+            if(arr[i] == value){
+                System.out.print("value found at " + i);
+                return i;
+            }
+        }
+        System.out.print("value not found");
+        return -1;
+    }
+    void delete(String value){
+        int location = search(value);
+        if (location == -1) return;
+        else {
+            arr[location] = arr[lastusedIndex];
+            lastusedIndex--;
+            System.out.println("The node successfully deleted ");
+        }
+    }
+    void DeleteBT(){
+        try{
+            arr=null;
+            System.out.println("Successfully Deleted Binary Tree");
+
+        } catch(Exception e){
+            System.out.println("Cann't Deleting Binary Tree");
+        }
     }
 }
