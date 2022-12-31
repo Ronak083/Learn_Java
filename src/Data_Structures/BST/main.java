63b0e0c123106bbd920cf818c072d4bdfd74204f
ronak <ronakgupta.official@gmail.com>
2022-12-31 15:40:16 +0530

    31/12 Insert in BST

---
+++ src/Data_Structures/BST/main.java
@@ -0,0 +1,26 @@
+package Data_Structures.BST;
+
+public class main {
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(1);
+        bst.insert(2);
+        bst.insert(3);
+        bst.insert(62);
+        bst.insert(23);
+        bst.insert(65);
+        bst.insert(21);
+        bst.insert(4);
+        bst.insert(6);
+        System.out.println();
+        bst.postorder(bst.root);
+        System.out.println();
+        bst.preorder(bst.root);
+        System.out.println();
+        bst.inorder(bst.root);
+        System.out.println();
+        bst.levelorder();
+        bst.Search(bst.root, 6);
+
+    }
+}
