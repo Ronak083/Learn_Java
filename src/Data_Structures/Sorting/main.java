c9f36c658699e92a4225babbce478cb06148ac53
ronak <ronakgupta.official@gmail.com>
2023-01-05 11:26:57 +0530

    05/01 MergeSort

--- src/Data_Structures/Sorting/main.java
+++ src/Data_Structures/Sorting/main.java
@@ -1,4 +1,9 @@
 package Data_Structures.Sorting;
 
 public class main {
+    public static void main(String[] args) {
+        int arr[] = {10,54,23,7,3,54,3,32,56,87,56,23,};
+        MergeSort.mergeSort(arr, 0, arr.length-1);
+        MergeSort.printArray(arr);
+    }
 }
