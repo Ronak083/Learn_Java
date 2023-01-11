f47ff5a53f8224af304c738bd5d3b7d25b0f7137
ronak <ronakgupta.official@gmail.com>
2023-01-11 11:14:01 +0530

    11/01 Sorting

--- src/Data_Structures/Sorting/InsertionSort.java
+++ src/Data_Structures/Sorting/InsertionSort.java
@@ -1,7 +1,15 @@
 package Data_Structures.Sorting;
 
 public class InsertionSort {
-    public static void InsertionSort(){
+    public static void InsertionSort(int[] arr){
+        for(int i =1;i<arr.length;i++){
+            int temp = arr[i], j=i;
+            while(j>0 && arr[j-1]>temp){
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = temp;
 
+        }
     }
 }
